a951ace8e6702144ce0886f628c52cc71681f633
kashyapdigitalyagnesh <kashyap.digitakyagnesh@gmail.com>
2025-05-30 11:41:37 +0530

    project set-up

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,20 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <h1 class="font-bold text-5xl font-title text-primary">
+        Title - Hello World!
+      </h1>
+      <h3 className="font-subtitle text-3xl font-semibold">
+        Sub Title - Meghal Shah CA
+      </h3>
+      <p className="text-gray">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id in delectus
+        inventore quos maiores numquam eius, at architecto laudantium tempora
+        deserunt corporis eos illo ratione expedita sequi perferendis dolor
+        voluptatem totam consequatur? Labore ex officiis culpa, natus nihil quae
+        adipisci consequatur fuga atque doloribus vel voluptates voluptatum
+        obcaecati autem facere?
+      </p>
     </>
   );
 }
